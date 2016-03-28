58cc9f4038428fdee3bd658d0799681408eafe54
Michael Cibulskis <Michael.Cibulskis@noackexpected.com>
2016-03-28 19:50:34 -0400

    starting with Jarvis-Patrick clustering

---
+++ src/test/scala/com/noackexpected/algorithm5/cluster/JPClusteringSpec.scala
@@ -0,0 +1,30 @@
+package com.noackexpected.algorithm5.cluster
+
+import org.scalatest._
+
+/**
+ * Copyright 2016 Michael J. Cibulskis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class JPClusteringSpec extends FlatSpec with Matchers {
+  "JPClustering" should "create no clusters when there are no items to cluster" in {
+    def target = new JPClustering
+    target.cluster(Set()).size should be (0)
+  }
+
+  it should "create no clusters when provided a null set of items to cluster" in {
+    def target = new JPClustering
+    target.cluster(null).size should be (0)
+  }
+}
